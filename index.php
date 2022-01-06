6c5b9a282052b18cf8f8640b472f3fea621e40e2
Nguyễn Tiến Đạt <tiendat.8498@gmail.com>
2022-01-06 20:14:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -245,12 +245,17 @@ if($ngay_d < 10){
 if($thang_d < 10){
 	$thang_d = '0'.$thang_d;
 }
+if(date('Y-m-d') <= $nam_d.'-'.$thang_d.'-'.$ngay_d){
+	echo $nam_show = date('Y');
+}else{
+	echo $nam_show = date('Y')+1;
+}
 $tet = $nam_d.'-'.$thang_d.'-'.$ngay_d.'T00:00:00';
 ?>
 <!DOCTYPE html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF8">
-    <title>Happy New Year <?=date('Y')+1?></title>    
+    <title>Happy New Year <?=$nam_show?></title>    
 	<meta content='width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0' name='viewport'/>
 	 <link href="images/icon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" meta name="theme-color" content="#880043">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -654,7 +659,7 @@ $tet = $nam_d.'-'.$thang_d.'-'.$ngay_d.'T00:00:00';
   <script src="lib/jquery.textillate.js"></script>
   
   <script>
-var canvas=document.getElementById("canvastext"),ctx=canvas.getContext("2d"),W=window.innerWidth,H=window.innerHeight,text="<?=date('Y')+1?>",text2="Chúc Mừng Năm Mới",skipCount=4,gravity=.2,touched=!1,mouse={},minDist=20,bounceFactor=.6;canvas.height=H;canvas.width=W;function trackPos(a){mouse.x=a.pageX;mouse.y=a.pageY}
+var canvas=document.getElementById("canvastext"),ctx=canvas.getContext("2d"),W=window.innerWidth,H=window.innerHeight,text="<?=$nam_show?>",text2="Chúc Mừng Năm Mới",skipCount=4,gravity=.2,touched=!1,mouse={},minDist=20,bounceFactor=.6;canvas.height=H;canvas.width=W;function trackPos(a){mouse.x=a.pageX;mouse.y=a.pageY}
 var Particle1=function(){this.r=6*Math.random();this.y=this.x=-100;this.vy=-5+parseInt(10*Math.random());this.vx=-5+parseInt(10*Math.random());this.isFree=!1;this.a=Math.random();this.draw=function(){ctx.beginPath();ctx.fillStyle="rgba(255, 223, 0, "+this.a+")";ctx.arc(this.x,this.y,this.r,0,2*Math.PI,!1);ctx.fill();ctx.closePath()};this.setPos=function(a,d){this.x=a;this.y=d}},particles1=[];
 (function(){ctx.fillStyle="black";ctx.font="100px Arial, sans-serif";ctx.textAlign="center";ctx.fillText(text,W/2,H/3);ctx.fillStyle="#29a1f1";ctx.font="70px Arial, sans-serif";ctx.fillText(text2,W/2,H/3+100)})();(function(){})();(function(){for(var a=ctx.getImageData(0,0,W,W),d=a.data,b=0;b<a.height;b+=skipCount)for(var c=0;c<a.width;c+=skipCount)255==d[c*a.width*4+4*b-1]&&(particles1.push(new Particle1),particles1[particles1.length-1].setPos(b,c))})();function clear(){ctx.clearRect(0,0,W,H)}
 function update1(){clear();for(i=0;i<particles1.length;i++){var a=particles1[i];a.r+=.15;a.a-=.015;0>a.a&&(a.r=6*Math.random(),a.a=Math.random());mouse.x>a.x-a.r&&mouse.x<a.x+a.r&&mouse.y>a.y-a.r&&mouse.y<a.y+a.r&&(touched=!0);1==touched&&(Math.sqrt((a.x-mouse.x)*(a.x-mouse.x)+(a.y-mouse.y)*(a.y-mouse.y))<=minDist&&(a.isFree=!0),1==a.isFree&&(a.y+=a.vy,a.x+=a.vx,a.vy+=gravity,a.y+a.r>H&&(a.vy*=-bounceFactor,a.y=H-a.r,a.vx=0<a.vx?a.vx-.1:a.vx+.1),a.x+a.r>W&&(a.vx*=-bounceFactor,a.x=W-a.r),0>a.x-a.r&&
